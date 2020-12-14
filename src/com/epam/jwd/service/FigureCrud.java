7935d30ef6766da51d673828a0772c8fee119a98
Роман Подстенный <70508104+romanoidprg@users.noreply.github.com>
2020-12-14 14:54:35 +0300

    Add files via upload

---
+++ src/com/epam/jwd/service/FigureCrud.java
@@ -0,0 +1,24 @@
+package com.epam.jwd.service;
+
+import com.epam.jwd.model.Figure;
+import com.epam.jwd.service.impl.FigureSearchCriterions;
+import java.util.Collection;
+import java.util.List;
+
+public interface FigureCrud {
+    void create(Figure figure);
+
+    void multiCreate(Collection<Figure> figureCollection);
+
+    void delete(int position);
+
+
+    void update(int position, Figure figure);
+
+
+    List<Figure> find(Figure figure);
+
+    Figure findById(int id);
+
+    List<Figure> findByCriterion(FigureSearchCriterions serchCriterions);
+}
